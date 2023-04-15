dd847000365de4d7cfea81f6123b5472bcdd7438
Fiodar Siyanka <siyankafiodar@gmail.com>
2023-04-15 14:57:54 +0300

    feat: hanoi.js carbon.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,8 +1,5 @@
 const { NotImplementedError } = require("../extensions/index.js");
 
-const MODERN_ACTIVITY = 15;
-const HALF_LIFE_PERIOD = 5730;
-
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
@@ -17,9 +14,26 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError("Not implemented");
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+const CONST_HALF_LIFE_PERIOD = 0.693;
+function dateSample(sampleActivity) {
+  // throw new NotImplementedError("Not implemented");
   // remove line with error and write your code here
+  let elemСoncentration = CONST_HALF_LIFE_PERIOD / HALF_LIFE_PERIOD;
+  let activity = +sampleActivity;
+  let log = Math.log(MODERN_ACTIVITY / activity);
+  let res = log / elemСoncentration;
+  if (
+    typeof sampleActivity !== "string" ||
+    sampleActivity <= 0 ||
+    sampleActivity > 15 ||
+    sampleActivity === undefined ||
+    isNaN(sampleActivity)
+  ) {
+    return false;
+  }
+  return Math.ceil(res);
 }
 
 module.exports = {
